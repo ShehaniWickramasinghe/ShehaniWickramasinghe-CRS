8966cb826ec659c356068c53370395e63c5382e8
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-01-29 12:47:50 +0530

    create login controller

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,36 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    
+    @FXML
+    private Hyperlink hyperlink;
+
+    @FXML
+    private Label lblLogin;
+
+    @FXML
+    private CheckBox rememberCheckBox;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperlinkClick(ActionEvent event) {
+
+    }
+}
