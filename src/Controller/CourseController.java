3a5536fed275b3fde642b170d57e1041ee82c5b8
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-01-29 08:13:58 +0530

    create course controller

---
+++ src/Controller/CourseController.java
@@ -0,0 +1,74 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CourseController {
+
+    @FXML
+    private ImageView imgTeacher;
+
+    @FXML
+    private Label lblClick;
+
+    @FXML
+    private Label lblCourseDetails;
+
+    @FXML
+    private Label lblCourseId;
+
+    @FXML
+    private Label lblCourseId1;
+
+    @FXML
+    private Label lblCreditHour;
+
+    @FXML
+    private Label lblDepartment;
+
+    @FXML
+    private Label lblMEC;
+
+    @FXML
+    private Label lblPrerequisities;
+
+    @FXML
+    private TextField txtCourseDetails;
+
+    @FXML
+    private TextField txtCourseDetails1;
+
+    @FXML
+    private ComboBox<?> txtDepartment;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtId1;
+
+    @FXML
+    private TextField txtMEC;
+
+    @FXML
+    private TextField txtPrerequisities;
+
+    @FXML
+    private TextField txtPrerequisities1;
+
+    @FXML
+    private TextField txtPrerequisities11;
+
+    @FXML
+    void btnGoBackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+}
