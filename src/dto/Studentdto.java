2a4a721de1fe71b8b1df610dfcdb6718e11eb18b
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-01-30 09:16:50 +0530

    create studentdto

---
+++ src/dto/Studentdto.java
@@ -0,0 +1,80 @@
+package dto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Studentdto {
+        private String studentId;
+        private String Name;
+        private LocalDate DOB;
+        private Integer phoneNumber;
+        private String email;
+        private ArrayList<String> programOfStudy;
+
+        public Studentdto(String studentId, String name, LocalDate dOB, Integer phoneNumber, String email,
+                ArrayList<String> programOfStudy) {
+            this.studentId = studentId;
+            Name = name;
+            DOB = dOB;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.programOfStudy = programOfStudy;
+        }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<String> getProgramOfStudy() {
+        return programOfStudy;
+    }
+
+    public void setProgramOfStudy(ArrayList<String> programOfStudy) {
+        this.programOfStudy = programOfStudy;
+    }
+
+    @Override
+    public String toString() {
+        return "Studentdto [studentId=" + studentId + ", Name=" + Name + ", DOB=" + DOB + ", phoneNumber=" + phoneNumber
+                + ", email=" + email + ", programOfStudy=" + programOfStudy + "]";
+    }
+
+    
+        
+}
