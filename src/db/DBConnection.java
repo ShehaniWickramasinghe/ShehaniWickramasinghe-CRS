b91ccb69423a0a99f6faef0cd371cd669164d149
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-01-30 08:59:57 +0530

    create dbconnection

---
+++ src/db/DBConnection.java
@@ -0,0 +1,26 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+        private static DBConnection dBConnection;
+        private static Connection connection;
+
+        private DBConnection() throws ClassNotFoundException,SQLException{
+             Class.forName("com.mysql.cj.jdbc.Driver");
+        connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", "shehani@2024");
+        }
+
+        private static DBConnection getInstance()throws Exception{
+            if (dBConnection==null) {
+                dBConnection=new DBConnection();
+            }
+            return dBConnection;
+        }
+
+        public  Connection getConnection(){
+            return this.connection;
+        }
+}
