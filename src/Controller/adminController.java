08ef5d7f27abb86c78a6466b981257de490dced0
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-07 14:35:18 +0530

    create admin.fxml

---
+++ src/Controller/adminController.java
@@ -0,0 +1,42 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class adminController {
+     @FXML
+    private Button btnCourse;
+
+    @FXML
+    private Button btnStudent;
+
+    @FXML
+    void btnCourseOnAction(ActionEvent event)throws Exception {
+            Stage stage=(Stage)btnCourse.getScene().getWindow();
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("../view/course.fxml"));
+            Parent root=loader.load();
+
+            Stage stage1=new Stage();
+            stage1.setScene(new Scene(root));
+            stage1.setTitle("Couse page");
+            stage1.show();
+            stage.close();
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event)throws Exception {
+        Stage stage=(Stage)btnStudent.getScene().getWindow();
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("../view/student.fxml"));
+        Parent root=loader.load();
+
+        Stage stage1=new Stage();
+        stage1.setScene(new Scene(root));
+        stage1.show();
+        stage.close();
+    }
+}
