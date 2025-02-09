229ca2c162b3939c9744e8d9ed562b853c2e8fc5
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-09 22:26:18 +0530

    implement savr,update and search

---
+++ src/dao/custom/impl/PrivateDaoImpl.java
@@ -0,0 +1,47 @@
+package dao.custom.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dao.custom.PrivateDao;
+import entity.PrivateEntity;
+
+public class PrivateDaoImpl implements PrivateDao {
+
+    @Override
+    public boolean save(PrivateEntity t) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public boolean update(PrivateEntity t) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public PrivateEntity search(String id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+
+    @Override
+    public List<PrivateEntity> searchAll(String id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchAll'");
+    }
+
+    @Override
+    public ArrayList<PrivateEntity> getAll() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+}
---
+++ src/service/custom/impl/PrivateServiceImpl.java
@@ -0,0 +1,53 @@
+package service.custom.impl;
+
+import dao.DaoFactory;
+import dao.custom.PrivateDao;
+import dto.Privatedto;
+import entity.PrivateEntity;
+import java.util.ArrayList;
+import service.custom.PrivateService;
+
+public class PrivateServiceImpl implements PrivateService{
+            private PrivateDao privateDao=(PrivateDao)DaoFactory.getInstance().getDao(DaoFactory.DaoType.PRIVATE);
+    @Override
+    public String save(Privatedto privatedto) throws Exception {
+        PrivateEntity privateEntity=new PrivateEntity(privatedto.getId(), privatedto.getName(),
+        privatedto.getDepartment(),privatedto.getAttendance(), privatedto.getSem1Grade(), privatedto.getSem2Grade());
+
+        boolean isSaved=privateDao.save(privateEntity);
+        return isSaved ? "Success":"fail";
+    }
+
+    @Override
+    public String update(Privatedto privatedto) throws Exception {
+        PrivateEntity privateEntity=new PrivateEntity(privatedto.getId(), privatedto.getName(), privatedto.getDepartment(),
+        privatedto.getAttendance(),privatedto.getSem1Grade(),privatedto.getSem2Grade());
+
+        boolean isUpdate=privateDao.update(privateEntity);
+        return isUpdate ? "Success":"fail";
+    }
+
+    @Override
+    public Privatedto search(String id) throws Exception {
+        PrivateEntity privateEntity=privateDao.search(id);
+        if (privateEntity!=null) {
+            return new Privatedto(privateEntity.getId(), privateEntity.getName(), privateEntity.getDepartment(), privateEntity.getAttendance(),
+            privateEntity.getSem1Grade(),privateEntity.getSem2Grade());
+
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Privatedto> getAll() throws Exception {
+       ArrayList<Privatedto> privatedtos=new ArrayList<>();
+       ArrayList<PrivateEntity> privateEntities=privateDao.getAll();
+       for (PrivateEntity privateEntity : privateEntities) {
+           Privatedto dto=new Privatedto(privateEntity.getId(),privateEntity.getName(),privateEntity.getDepartment(),privateEntity.getAttendance(),
+           privateEntity.getSem1Grade(),privateEntity.getSem2Grade());
+           privatedtos.add(dto);
+       }
+       return privatedtos;
+    }
+
+}
