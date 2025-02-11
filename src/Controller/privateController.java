d5dff41c6127385d3c814d550c991ed4614ec9b3
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-11 11:28:28 +0530

    created  getAllDetails

---
+++ src/Controller/privateController.java
@@ -0,0 +1,177 @@
+package  controller;
+
+import dto.Privatedto;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableView;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import service.custom.PrivateService;
+import service.custom.impl.PrivateServiceImpl;
+
+public class privateController {
+     @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<Privatedto, String> colAttendance;
+
+    @FXML
+    private TableColumn<Privatedto, String> colDepartment;
+
+    @FXML
+    private TableColumn<Privatedto, String> colId;
+
+    @FXML
+    private TableColumn<Privatedto, String> colName;
+
+    @FXML
+    private TableColumn<Privatedto, String> colSem1;
+
+    @FXML
+    private TableColumn<Privatedto, String> colSem2;
+
+    @FXML
+    private Label lblAttendance;
+
+    @FXML
+    private Label lblDepartment;
+
+    @FXML
+    private Label lblGrade2;
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private Label lblSem1;
+
+    @FXML
+    private Label lblStudentId;
+
+    @FXML
+    private TableView<Privatedto> tblAcademic;
+
+    @FXML
+    private TextField txtAttendance;
+
+    @FXML
+    private TextField txtDepartment;
+
+    @FXML
+    private TextField txtGrade1;
+
+    @FXML
+    private TextField txtGrade2;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtStudentId;
+
+    public void initialize() throws Exception{
+        getAllDetails();
+        colId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        colDepartment.setCellValueFactory(new PropertyValueFactory<>("Department"));
+        colAttendance.setCellValueFactory(new PropertyValueFactory<>("Attendance"));
+        colSem1.setCellValueFactory(new PropertyValueFactory<>("Sem1Grade"));
+        colSem2.setCellValueFactory(new PropertyValueFactory<>("Sem2Grade"));
+        
+        tblAcademic.getSelectionModel().selectedItemProperty().addListener((obs,oldSelection,newSelection)->{
+            System.out.println("Table row click");
+            System.out.println("oldSelection:"+oldSelection);
+            System.out.println("=====================");
+            System.out.println("newSelection:"+newSelection);
+            if (newSelection!=null) {
+                txtStudentId.setText(newSelection.getId());
+                txtName.setText(newSelection.getName());
+                txtDepartment.setText(newSelection.getDepartment());
+                txtAttendance.setText(newSelection.getAttendance());
+                txtGrade1.setText(newSelection.getSem1Grade());
+                txtGrade2.setText(newSelection.getSem2Grade());
+            }
+        });
+        
+    }
+
+    public void getAllDetails() throws Exception{
+        PrivateService privateService=new PrivateServiceImpl();
+        ArrayList<Privatedto> getAll=privateService.getAll();
+        System.out.println(getAll);
+        ObservableList<Privatedto> observableList=FXCollections.observableArrayList();
+        observableList.addAll(getAll);
+        tblAcademic.setItems(observableList);
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) throws Exception {
+            String Id = txtStudentId.getText();
+            String Name = txtName.getText();
+            String Department = txtDepartment.getText();
+            String Attendance = txtAttendance.getText();
+            String sem1 = txtGrade1.getText();
+            String sem2 = txtGrade2.getText();
+
+            Privatedto privatedto=new Privatedto(Id, Name, Department, Attendance, sem1, sem2);
+            PrivateService privateService=new PrivateServiceImpl();
+            String save = privateService.save(privatedto);
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) throws Exception {
+                    String Id = txtStudentId.getText();
+                   PrivateService privateService=new PrivateServiceImpl();
+                   Privatedto privatedto = privateService.search(Id);
+                    if (privatedto!=null) {
+                        txtStudentId.setText(privatedto.getId());
+                        txtName.setText(privatedto.getName());
+                        txtDepartment.setText(privatedto.getDepartment());
+                        txtAttendance.setText(privatedto.getAttendance());
+                        txtGrade1.setText(privatedto.getSem1Grade());
+                        txtGrade2.setText(privatedto.getSem2Grade());
+                    }else{
+                        Alert alert=new Alert(Alert.AlertType.INFORMATION,"Student Not Found",ButtonType.OK);
+                    }
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) throws Exception {
+            String Id = txtStudentId.getText();
+            String Name = txtName.getText();
+            String Department = txtDepartment.getText();
+            String Attendance = txtAttendance.getText();
+            String sem1 = txtGrade1.getText();
+            String sem2 = txtGrade2.getText();
+
+            Privatedto privatedto=new Privatedto(Id, Name, Department, Attendance, sem1, sem2);
+            PrivateService privateService=new PrivateServiceImpl();
+            String update = privateService.update(privatedto);
+    }
+
+    public void clearForm(){
+        txtStudentId.setText("");
+        txtName.setText("");
+        txtDepartment.setText("");
+        txtAttendance.setText("");
+        txtGrade1.setText("");
+        txtGrade2.setText("");
+    }
+}
