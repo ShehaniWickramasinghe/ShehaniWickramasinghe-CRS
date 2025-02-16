0b5f359d30ead6ab4dff7b507cc22ca040a8a8c3
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-16 22:21:22 +0530

    search method process

--- src/Controller/CheckController.java
+++ src/Controller/CheckController.java
@@ -1,20 +1,21 @@
 package controller;
 
-import java.awt.Cursor;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 
+import dto.Checkdto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import service.custom.CheckService;
+import service.custom.impl.CheckServiceImpl;
 
 public class CheckController {
     @FXML
@@ -77,8 +78,17 @@ public class CheckController {
 
     
     @FXML
-    void bthSearchOnAction(ActionEvent event) {
-
+    void bthSearchOnAction(ActionEvent event) throws Exception {
+            String name = txtCourseName.getText();
+            CheckService checkService=new CheckServiceImpl();
+            Checkdto checkdto = checkService.search(name);
+            if (checkdto!=null) {
+                txtCourseName.setText(checkdto.getName());
+                lblTextPrerequisites.setText(checkdto.getPrerequisites());
+            }else{
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Course not found!", ButtonType.OK);
+                 alert.showAndWait();
+            }
     }
 
     @FXML
@@ -86,12 +96,11 @@ public class CheckController {
             Stage stage=(Stage)btnNo.getScene().getWindow();
              FXMLLoader loader=new FXMLLoader(getClass().getResource("../view/report.fxml"));
             Parent root=loader.load();
+            
+                        Alert alert=new Alert(Alert.AlertType.WARNING,"This course can not allow you to register");
+                        alert.showAndWait();
 
-            Stage stage1=new Stage();
-            stage1.setScene(new Scene(root));
-            stage1.show();
-            stage.close();
-
+           stage.close();
     }
 
     @FXML
