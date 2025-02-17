4285705316523bddc5dbc8a95e23854447e16529
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-17 18:47:59 +0530

    change check username get a database

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import db.DBConnection;
+import dto.Logindto;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,6 +18,8 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import service.custom.LoginService;
+import service.custom.impl.LoginServiceImpl;
 
 public class LoginController {
     @FXML
@@ -47,74 +50,85 @@ public class LoginController {
         currentStage.close();
     }
 
+    
     @FXML
     void btnSignInOnAction(ActionEvent event) throws IOException {
-        String user_name=txtUserName.getText();
-        String password=txtPassword.getText();
-        if (authenticateUser(user_name, password)) {
-            System.out.println("Login successful!");
-            try {
-                Stage stage=(Stage)btnSignIn.getScene().getWindow();
-                FXMLLoader loader;
-                if (user_name.matches("A00[0-9]") || user_name.matches("C00[0-9]") || user_name.matches("C0(10|11|12|13|14|15|16|17|18|19|20)")) {
-                    loader=new FXMLLoader(getClass().getResource("../view/admin.fxml"));
-                    Parent root1=loader.load();
+     String user_name = txtUserName.getText();
+        String password = txtPassword.getText();
+    
+    if (authenticateUser(user_name, password)) {
+        System.out.println("Login successful!");
+        try {
+            Stage stage = (Stage) btnSignIn.getScene().getWindow();
+            FXMLLoader loader;
 
-                    Stage stage2=new Stage();
-                    stage2.setScene(new Scene(root1));
-                    stage2.setTitle("Admin Page");
-                    stage2.show();
-                    stage.close();
-                }else if(user_name.matches("S00[0-9]")){
-                    loader=new FXMLLoader(getClass().getResource("../view/report.fxml"));
-                    Parent root=loader.load();
-             
-                    Stage stage1=new Stage();
-                    stage1.setScene(new Scene(root));
-                    stage1.setTitle("Student Report Page");
-                     stage1.show();
-                     stage.close();
-                }
-        
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("..................");
-                
-            }
-        } else {
-            System.out.println("Invalid username or password.");
-            Alert alert=new Alert(Alert.AlertType.ERROR, "The username or password is incorrect");
-            alert.showAndWait();
-            clearForm();
-        }
-    }
-
-    public static boolean authenticateUser(String user_name,String password){
-        String query="SELECT password FROM login WHERE user_name=?";
-        System.out.println("=============================");
-        try (Connection connection=DBConnection.getInstance().getConnection();
-        PreparedStatement statement=connection.prepareStatement(query))
-            {   
-                  statement.setString(1, user_name);
-            ResultSet rst=statement.executeQuery();
-
-            while (rst.next()) {
-                String storedHash = rst.getString("password");
-                if (password.equals(storedHash)) {
-                    return true;
-                }
             
-              } 
-           
+            if (isAdmin(user_name)) {
+                loader = new FXMLLoader(getClass().getResource("../view/admin.fxml"));
+                Parent root1 = loader.load();
+                Stage stage2 = new Stage();
+                stage2.setScene(new Scene(root1));
+                stage2.setTitle("Admin Page");
+                stage2.show();
+                stage.close();
+            } else if (isStudent(user_name)) {
+                loader = new FXMLLoader(getClass().getResource("../view/report.fxml"));
+                Parent root = loader.load();
+                Stage stage1 = new Stage();
+                stage1.setScene(new Scene(root));
+                stage1.setTitle("Student Report Page");
+                stage1.show();
+                stage.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("Error navigating to the page.");
         }
-        System.out.println("=============================");
-        return false;
+    } else {
+        System.out.println("Invalid username or password.");
+        Alert alert = new Alert(Alert.AlertType.ERROR, "The username or password is incorrect");
+        alert.showAndWait();
+        clearForm();
     }
+}
+
+
+public static boolean authenticateUser(String user_name, String password) {
+    String query = "SELECT password FROM login WHERE user_name=?";
+    try (Connection connection = DBConnection.getInstance().getConnection();
+         PreparedStatement statement = connection.prepareStatement(query)) {
+
+        statement.setString(1, user_name);
+        ResultSet rst = statement.executeQuery();
+
+        if (rst.next()) {
+            String storedHash = rst.getString("password");
+            if (password.equals(storedHash)) {
+                return true; 
+            }
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+        return false; 
+    }
+
+
+        private boolean isAdmin(String user_name) {
+            return user_name.startsWith("A") || user_name.startsWith("C"); 
+        }
+
+
+        private boolean isStudent(String user_name) {
+                return user_name.startsWith("S"); 
+        }       
+
         public void clearForm(){
             txtUserName.setText("");
             txtPassword.setText("");
         }
+
+        
         
 }
