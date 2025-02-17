07a0f286b64b7cf3fa1b9b0a75e22339a4ae176a
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-17 18:48:31 +0530

    create loginEntity and logindto

---
+++ src/entity/LoginEntity.java
@@ -0,0 +1,33 @@
+package entity;
+
+public class LoginEntity {
+    private String name;
+    private String password;
+
+    public LoginEntity(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginEntity [name=" + name + ", password=" + password + "]";
+    }
+
+}
