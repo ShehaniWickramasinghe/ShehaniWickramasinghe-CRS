e7bd4f81a23eb0a87cd0f9ce1c054572e2a821cc
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-17 18:51:42 +0530

    create login and method

---
+++ src/dao/custom/impl/LoginDaoImpl.java
@@ -0,0 +1,50 @@
+package dao.custom.impl;
+
+import dao.CrudUtil;
+import dao.custom.LoginDao;
+import entity.LoginEntity;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginDaoImpl implements LoginDao {
+
+    @Override
+    public boolean save(LoginEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO login VALUES(?,?)",t.getName(),t.getPassword());
+    }
+
+    @Override
+    public boolean update(LoginEntity t) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean delete(String id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public LoginEntity search(String id) throws Exception {
+       ResultSet rst=CrudUtil.extecuteQuery("SELECT * FROM login WHERE user_name=?",id);
+       if (rst.next()) {
+        return new LoginEntity(rst.getString("user_name"), rst.getString("password"));
+       }
+       return null;
+    }
+
+    @Override
+    public List<LoginEntity> searchAll(String id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchAll'");
+    }
+
+    @Override
+    public ArrayList<LoginEntity> getAll() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+}
---
+++ src/service/custom/LoginService.java
@@ -0,0 +1,10 @@
+package service.custom;
+
+import dto.Logindto;
+import service.SuperService;
+
+public interface LoginService extends SuperService{
+        public String save(Logindto logindto)throws Exception; 
+        public Logindto search(String name)throws Exception;
+}       
+        
