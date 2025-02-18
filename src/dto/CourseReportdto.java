5197d7fecc85b47844da821e4c96667a5c8dc8cf
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-18 17:53:30 +0530

    create Coursereport dto

---
+++ src/dto/CourseReportdto.java
@@ -0,0 +1,66 @@
+package dto;
+
+public class CourseReportdto {
+        private String name;
+        private int enrolledStudent;
+        private int completedStudent;
+        private String performance;
+        private String rate;
+        
+        public CourseReportdto(String name, int enrolledStudent, int completedStudent, String performance,
+                String rate) {
+            this.name = name;
+            this.enrolledStudent = enrolledStudent;
+            this.completedStudent = completedStudent;
+            this.performance = performance;
+            this.rate = rate;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getEnrolledStudent() {
+            return enrolledStudent;
+        }
+
+        public void setEnrolledStudent(int enrolledStudent) {
+            this.enrolledStudent = enrolledStudent;
+        }
+
+        public int getCompletedStudent() {
+            return completedStudent;
+        }
+
+        public void setCompletedStudent(int completedStudent) {
+            this.completedStudent = completedStudent;
+        }
+
+        public String getPerformance() {
+            return performance;
+        }
+
+        public void setPerformance(String performance) {
+            this.performance = performance;
+        }
+
+        public String getRate() {
+            return rate;
+        }
+
+        public void setRate(String rate) {
+            this.rate = rate;
+        }
+
+        @Override
+        public String toString() {
+            return "CourseReportdto [name=" + name + ", enrolledStudent=" + enrolledStudent + ", completedStudent="
+                    + completedStudent + ", performance=" + performance + ", rate=" + rate + "]";
+        }
+
+        
+}
