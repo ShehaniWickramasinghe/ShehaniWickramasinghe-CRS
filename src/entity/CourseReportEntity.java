f7a4f98999fdbf5f031a6105c3f7be83b8b0b2a3
shehani wickramasinghe <shehaniwathsalawi@gmail.com>
2025-02-18 17:53:56 +0530

    create CourseReport entity

---
+++ src/entity/CourseReportEntity.java
@@ -0,0 +1,65 @@
+package entity;
+
+public class CourseReportEntity {
+    private String name;
+    private int enrolledStudent;
+    private int completedStudent;
+    private String performance;
+    private String rate;
+    
+    public CourseReportEntity(String name, int enrolledStudent, int completedStudent, String performance, String rate) {
+        this.name = name;
+        this.enrolledStudent = enrolledStudent;
+        this.completedStudent = completedStudent;
+        this.performance = performance;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEnrolledStudent() {
+        return enrolledStudent;
+    }
+
+    public void setEnrolledStudent(int enrolledStudent) {
+        this.enrolledStudent = enrolledStudent;
+    }
+
+    public int getCompletedStudent() {
+        return completedStudent;
+    }
+
+    public void setCompletedStudent(int completedStudent) {
+        this.completedStudent = completedStudent;
+    }
+
+    public String getPerformance() {
+        return performance;
+    }
+
+    public void setPerformance(String performance) {
+        this.performance = performance;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "CourseReportEntity [name=" + name + ", enrolledStudent=" + enrolledStudent + ", completedStudent="
+                + completedStudent + ", performance=" + performance + ", rate=" + rate + "]";
+    }
+
+    
+}
